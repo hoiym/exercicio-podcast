dcd8f8e578331a366085f946d3cb83061abd28c0
hoiym <hym@cin.ufpe.br>
2017-09-30 11:12:05 -0300

    Items 1 and 2

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/domain/XmlFeedParser.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/domain/XmlFeedParser.java
@@ -1,5 +1,8 @@
 package br.ufpe.cin.if710.podcast.domain;
 
+import android.util.Log;
+import android.widget.Toast;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -114,8 +117,8 @@ public class XmlFeedParser {
     // Processa tags do tipo <enclosure> para obter dados do episodio
     public static String readEnclosure(XmlPullParser parser)
             throws IOException, XmlPullParserException {
-        //parser.require(XmlPullParser.START_TAG, null, "enclosure");
-        String data = "implementar...";
+        parser.require(XmlPullParser.START_TAG, null, "enclosure");
+        String data = parser.getAttributeValue(null, "url");
         //parser.require(XmlPullParser.END_TAG, null, "enclosure");
         return data;
     }
